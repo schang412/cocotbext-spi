c6e996e85382dbef3d2b9ae9385eed8fd8d50f7d
Spencer Chang <schang412@gmail.com>
2023-02-10 01:11:13 -0800

    fix regression where cocotbext-spi fails for py37

--- cocotbext/spi/about.py
+++ cocotbext/spi/about.py
@@ -1,6 +1,14 @@
-from importlib.metadata import version, PackageNotFoundError
-
 try:
-    __version__ = version("cocotbext-spi")
-except PackageNotFoundError:
-    pass
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from pkg_resources import get_distribution, DistributionNotFound
+    try:
+        __version__ = get_distribution("cocotbext-spi").version
+    except DistributionNotFound:
+        __version__ = "0.0.0"
+else:
+    try:
+        __version__ = version("cocotbext-spi")
+    except PackageNotFoundError:
+        __version__ = "0.0.0"
+
