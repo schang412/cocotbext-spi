e9b9f612f61b501177d8326dcec73ec741460de3
Richard Jones <richard.jones@etlsystems.com>
2026-07-08 10:31:24 +0100

    Further changed 2 instances of 'unit' to 'units'

--- cocotbext/spi/spi.py
+++ cocotbext/spi/spi.py
@@ -418,8 +418,8 @@ class SpiSlaveBase(ABC):
 
 class _SpiClock:
     def __init__(self, signal, period, units="step", start_high=True):
-        self.period = cocotb.utils.get_sim_steps(period, unit, round_mode="round")
-        self.half_period = cocotb.utils.get_sim_steps(period / 2.0, unit, round_mode="round")
+        self.period = cocotb.utils.get_sim_steps(period, units, round_mode="round")
+        self.half_period = cocotb.utils.get_sim_steps(period / 2.0, units, round_mode="round")
         self.frequency = 1.0 / cocotb.utils.get_time_from_sim_steps(self.period, units='us')
 
         self.signal = signal
